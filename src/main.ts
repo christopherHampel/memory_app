3a4df09aed8e38cbe6d074ad65a4e7d0d1b55800
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-03-04 20:51:18 +0100

    start with the start-screen

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
-import './styles/style.scss';
+import './scss/main.scss';
 
-document.getElementById('test')!.innerText = "gude"
+// init()
+
+// function init(){
+//     const fieldRef = document.getElementById('field');
+
+//     if(fieldRef) {
+//         fieldRef.addEventListener('click', e => {
+//             const card = (e.target as HTMLElement ).closest(".card") as HTMLButtonElement;
+//             if(card) {
+//                 card.classList.toggle('is-flipped')
+//             }
+//         })
+//     }
+// }
